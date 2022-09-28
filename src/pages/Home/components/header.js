4987606d5ed2445c34f166689a3addbb451df69d
João Pedro <joaopedromalagi@hotmail.com>
2022-09-28 19:07:57 -0300

    Initial Code

---
+++ src/pages/Home/components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className='header'>
+              <div className='tollbar'>
+                <span>Conecta Dev</span>              
+              <div>
+                <button>Novo Post</button>
+                <span>img1</span>
+                <span>img2</span>
+                </div>
+              </div>
+            </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import Header from './components/header';
+import './style.css';
+
+function Home() {
+    return(
+        <div>
+            <Header />
+            <main className='main'>
+              <div className='navbar'>
+                navbar
+              </div>
+              <div className='feed'>
+                feed
+              </div>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
